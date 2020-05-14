8bb4a779827d79ce8cb228a247b99eef6031b01c
Kane Kennedy <44435580+Zochonis@users.noreply.github.com>
2020-05-14 08:25:14 +0100

    Chapter 1 - Solutions
    These files are the solutions for the exercises in the 1st chapter.

---
+++ ThinkJava_CH1/Ex1_2.java
@@ -0,0 +1,12 @@
+public class Ex1_2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+		System.out.println("How are you?");
+		
+		// This is a comment.
+
+	}
+
+}
