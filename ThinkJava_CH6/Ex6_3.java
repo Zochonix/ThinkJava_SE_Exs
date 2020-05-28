b2824cd42b7711d54d14e8d9a6587fc960a7e96a
Kane Kennedy <44435580+Zochonis@users.noreply.github.com>
2020-05-28 22:24:55 +0100

    Chapter 6 - Solutions
    This one is a work in progress but I still thought I'd upload it.

---
+++ ThinkJava_CH6/Ex6_3.java
@@ -0,0 +1,26 @@
+public class Ex6_3 {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		int n = 1;
+		
+		System.out.println("This program that returns the sum of the first n terms of this ")
+		
+		gauss(x, n);
+
+	}
+	
+	public static void gauss(int x, int n) {
+		
+		int sum;
+		
+		for (int i = 0; i == n; i++) {
+			
+			
+			
+		}
+		
+	}
+
+}
