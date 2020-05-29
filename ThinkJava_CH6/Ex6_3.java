7b3189aebec0ff8f703bdd4a88057177bebcc9c8
Kane Kennedy <44435580+Zochonis@users.noreply.github.com>
2020-05-29 22:42:07 +0100

    Chapter 6 - Solutions
    Added comments to Ex6_3.

---
+++ ThinkJava_CH6/Ex6_3.java
@@ -0,0 +1,37 @@
+// No class will be imported for this program.
+
+public class Ex6_3 {
+
+	public static void main(String[] args) {
+		
+		// Declare the required variables.
+		
+		int x = 1;
+		int n = 1;
+		
+		// Inform the user, of what the program will be doing.
+		
+		System.out.println("This program, returns the sum of the first n terms of this expression.");
+		
+		gauss(x, n);
+
+	}
+	
+	public static void gauss(int x, int n) {
+		
+		int sum;
+		
+		// The program will loop thre number of tiemes as is necessary
+		// for the operation tobe completed, so it will loop n times.
+		
+		// Remember ITO.
+		
+		for (int i = 0; i == n; i++) {
+			
+			
+			
+		}
+		
+	}
+
+}
